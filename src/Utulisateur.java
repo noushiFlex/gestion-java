eb569f86bdc1fc8bdd31c2e123f71c5f6f70478c
noushiFlex <yohannvessime@gmail.com>
2025-03-11 12:11:17 +0000

    Ajout de la page principale et de l'utulisateur

---
+++ src/Utulisateur.java
@@ -0,0 +1,16 @@
+
+class Utilisateur {
+    private String nom;
+    private String motDePasse;
+
+    // Constructeur pour initialiser les valeurs
+    public Utilisateur(String nom, String motDePasse) {
+        this.nom = nom;
+        this.motDePasse = motDePasse;
+    }
+
+    // Méthode pour vérifier l'authentification
+    public boolean authentification() {
+        return this.nom.equals("admin") && this.motDePasse.equals("admin");
+    }
+}
